a2ae07e86c58e250543254c44e12f1d68ce9a73f
Mgen <m@mgenware.com>
2024-08-02 02:59:56 +0800

    Remove some redundant code

--- lib/fc_native_image_resize_method_channel.dart
+++ lib/fc_native_image_resize_method_channel.dart
@@ -19,9 +19,6 @@ class MethodChannelFcNativeImageResize extends FcNativeImageResizePlatform {
       required String format,
       bool? srcFileUri,
       int? quality}) async {
-    if (width <= 0 && height <= 0) {
-      throw ArgumentError('Both size and height cannot be 0');
-    }
     await methodChannel.invokeMethod<void>('resizeFile', {
       'srcFile': srcFile,
       'destFile': destFile,
